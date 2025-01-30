b676dc75cff3170a01b7394ab8814762fb1ec889
Levi <shekhar.3706@gmail.com>
2025-01-30 23:43:45 +0530

    proper error messages and bug fix for enum values converted to numbers causing error in switch evaluation

--- src/index.ts
+++ src/index.ts
@@ -65,31 +65,26 @@ const ALLOWED_DAYS = [
 	'saturday',
 ];
 
-enum DAYS {
-	'sunday' = 1,
-	'monday' = 2,
-	'tuesday' = 3,
-	'wednesday' = 4,
-	'thursday' = 5,
-	'friday' = 6,
-	'saturday' = 7,
-}
+const DAYS = new Map([
+	['sunday', 1],
+	['monday', 2],
+	['tuesday', 3],
+	['wednesday', 4],
+	['thursday', 5],
+	['friday', 6],
+	['saturday', 7],
+]);
+
 export default class LambdaCronJobs {
 	functions: Functions;
 	service: any;
 	provider: any;
-	log: any;
 	pluginConfig: PluginConfiguration;
 	hooks: Hooks;
 	stage: string;
 	config: any;
 
-	constructor(
-		serverless: Serverless,
-		options: Serverless.Options,
-		{ log }: any
-	) {
-		this.log = log;
+	constructor(serverless: Serverless, options: Serverless.Options) {
 		this.service = serverless.service;
 		this.functions = serverless.service?.functions;
 		this.stage = options?.stage ?? serverless.service.provider?.stage;
@@ -109,12 +104,11 @@ export default class LambdaCronJobs {
 	private beforePackage() {
 		try {
 			if (!this.hasCronJobs()) {
-				this.log(`No cron job configurations found for stage ${this.stage}`);
+				console.log(`No cron job configurations found for stage ${this.stage}`);
 				return;
 			}
 
 			this.getStageConfig();
-			this.log(this.config);
 
 			for (let functionName in this.config) {
 				this.addCronSchedule(functionName, this.config[functionName]);
@@ -133,6 +127,10 @@ export default class LambdaCronJobs {
 		const cronSchedules: Aws.Event[] = this.getScheduleEvent(cronJobConfig);
 		// Added scheduled event to lambda
 		currentFunction.events = [...currentFunction.events, ...cronSchedules];
+		console.log('scheduled cron for: ', {
+			function: functionName,
+			schedule: cronJobConfig.schedule,
+		});
 	}
 
 	private getScheduleEvent(cronJobConfig: PluginConfigurationObject) {
@@ -150,19 +148,19 @@ export default class LambdaCronJobs {
 
 	private schedule(schedule: CronScheduleBase) {
 		if (!schedule) throw new Error('schedule can not be empty');
-
-		switch (schedule.type) {
-			case CrontType.interval:
+		const schedueType = schedule.type.toString();
+		switch (schedueType) {
+			case 'interval':
 				return this.scheduleInterval(schedule.params as Interval);
-			case CrontType.daily:
+			case 'daily':
 				return this.scheduleDaily(schedule.params as Daily);
-			case CrontType.weekly:
+			case 'weekly':
 				return this.scheduleWeekly(schedule.params as Weekly);
-			case CrontType.monthly:
+			case 'monthly':
 				return this.scheduleMonthly(schedule.params as Monthly);
 			default:
 				throw new Error(
-					'Invalid Type: cron can be scheduled with only given types: "interval", "daily", "weekly", "monthly"'
+					'Invalid schedule type: cron can be scheduled with only given types: "interval", "daily", "weekly", "monthly".'
 				);
 		}
 	}
@@ -170,17 +168,17 @@ export default class LambdaCronJobs {
 	private scheduleInterval(interval: Interval) {
 		if (!interval?.unit || !interval?.duration)
 			throw new Error(
-				'both "unit", "duration" are required for interval schedule'
+				'Missing param: both "unit", "duration" are required for interval schedule'
 			);
-		else if (interval.unit && typeof interval.unit != 'string')
-			throw new Error('Type Error: interval unit must be a number');
+		else if (typeof interval.unit != 'string')
+			throw new Error('Invalid param: interval unit must be a number');
 		else if (!ALLOWED_INTERVAL_UNIT.includes(interval.unit))
 			throw new Error(
-				'Invalid Unit: Invalid Unit provided.valid units are: ' +
+				'Invalid param: Invalid unit provided. Valid units are: ' +
 					ALLOWED_INTERVAL_UNIT.toString()
 			);
-		else if (interval.duration && typeof interval.duration != 'number')
-			throw new Error('Type Error: interval duration must be a number');
+		else if (typeof interval.duration != 'number')
+			throw new Error('Invalid param: interval duration must be a number');
 
 		return `rate(${interval.duration} ${interval.unit})`;
 	}
@@ -188,13 +186,19 @@ export default class LambdaCronJobs {
 	private scheduleDaily = (daily: Daily) => {
 		if (!daily?.hour)
 			throw new Error('Missing param: hour is required for daily schedule');
-
-		if (typeof daily.hour != 'number')
-			throw new Error('Type Error: hour must be a number');
-
-		if (daily.minute) {
-			if (typeof daily.minute != 'number')
-				throw new Error('Type Error: minute must be a number');
+		else if (typeof daily.hour != 'number' || daily.hour < 0 || daily.hour > 24)
+			throw new Error(
+				'Invalid param: hour must be a number be between 0 and 24'
+			);
+		else if (daily.minute) {
+			if (
+				typeof daily.minute != 'number' ||
+				daily.minute < 0 ||
+				daily.minute > 60
+			)
+				throw new Error(
+					'Invalid param: minute must be a number between 0 and 60'
+				);
 		} else {
 			console.log('minute is not provided in params default value is set to 0');
 		}
@@ -206,32 +210,43 @@ export default class LambdaCronJobs {
 
 	private scheduleWeekly(weekly: Weekly) {
 		if (!weekly?.day)
-			throw new Error('day is required for Weekly schedule to at given day');
+			throw new Error(
+				'day is required for Weekly schedule to schedule for given day'
+			);
 
 		if (typeof weekly.day != 'string')
-			throw new Error('Type Error: day must be a string');
+			throw new Error('Invalid param: day must be a string');
 
 		if (!ALLOWED_DAYS.includes(weekly.day.toLowerCase()))
 			throw new Error(
 				'Invalid param: invalid day passed. Allowed values are: ' +
 					ALLOWED_DAYS.toString()
 			);
+
 		if (weekly.hour) {
-			if (typeof weekly.hour != 'number')
-				throw new Error('Type Error: hour must be a number');
+			if (typeof weekly.hour != 'number' || weekly.hour < 0 || weekly.hour > 24)
+				throw new Error(
+					'Invalid param: hour must be a number be between 0 and 24'
+				);
 		} else
 			console.log('hour is not provided in params default value is set to 0');
 
 		if (weekly.minute) {
-			if (typeof weekly.minute != 'number')
-				throw new Error('Type Error: minute must be a number');
+			if (
+				typeof weekly.minute != 'number' ||
+				weekly.minute < 0 ||
+				weekly.minute > 60
+			)
+				throw new Error(
+					'Invalid param: minute must be a number between 0 and 60'
+				);
 		} else
 			console.log('minute is not provided in params default value is set to 0');
 
-		const day = weekly.day;
+		const day = weekly.day.toLowerCase();
 		const hours = weekly?.hour ?? 0;
 		const minutes = weekly?.minute ?? 0;
-		return `cron(${minutes} ${hours} ? * ${day} *)`;
+		return `cron(${minutes} ${hours} ? * ${DAYS} *)`;
 	}
 
 	private scheduleMonthly(monthly: Monthly) {
@@ -240,23 +255,32 @@ export default class LambdaCronJobs {
 				'day is required for monthly schedule to schedule for a given day'
 			);
 
-		if (typeof monthly.day != 'number')
-			throw new Error('Type Error: day must be a number for monthly schedule');
-
-		if (monthly.day < 0 || monthly.day > 31)
+		if (typeof monthly.day != 'number' || monthly.day < 0 || monthly.day > 31)
 			throw new Error(
-				'Invalid param: invalid day passed. Allowed values are: ' +
-					ALLOWED_DAYS.toString()
+				'Invalid param: day must be a number for monthly schedule between 0 and 60'
 			);
-		if (monthly.hour) {
-			if (typeof monthly.hour != 'number')
-				throw new Error('Type Error: hour must be a number');
+
+		if (monthly?.hour) {
+			if (
+				typeof monthly.hour != 'number' ||
+				monthly.hour < 0 ||
+				monthly.hour > 24
+			)
+				throw new Error(
+					'Invalid param: hour must be a number be between 0 and 24'
+				);
 		} else
 			console.log('hour is not provided in params default value is set to 0');
 
 		if (monthly.minute) {
-			if (typeof monthly.minute != 'number')
-				throw new Error('Type Error: minute must be a number');
+			if (
+				typeof monthly.minute != 'number' ||
+				monthly.minute < 0 ||
+				monthly.minute > 60
+			)
+				throw new Error(
+					'Invalid param: minute must be a number between 0 and 60'
+				);
 		} else
 			console.log('minute is not provided in params default value is set to 0');
 
